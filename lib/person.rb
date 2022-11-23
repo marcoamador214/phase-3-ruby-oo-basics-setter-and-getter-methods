40bb1d638c7a6ff4aaf332102c157a8cfaad7392
Marco Amador <marcoamador214@gmail.com>
2022-11-23 08:35:27 -0800

    commit tree b03608b1b010a13f8bc66cd18d9f456f186543b7

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
+#Names
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+#Jobs
+    def job=(job)
+        @job = job
+    end
+
+    def job
+        @job
+    end
 
 end
